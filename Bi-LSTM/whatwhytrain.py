22cbd440467ce91fb65911c66aba53311397597e
Sahil Saini <sainisahil432@gmail.com>
2025-10-04 16:25:48 -0400

    added new script to get new diffs from gtihub repos, added some comments

--- Bi-LSTM/whatwhytrain.py
+++ Bi-LSTM/whatwhytrain.py
@@ -1,5 +1,4 @@
-
-from torchmetrics.functional import accuracy, recall, precision, f1_score  # lightning中的评估
+from torchmetrics.functional import accuracy, recall, precision, f1_score  # evaluation metrics in lightning
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning.callbacks import ModelCheckpoint
 import torch
@@ -13,12 +12,15 @@ from torch.nn.functional import one_hot
 import pytorch_lightning as pl
 from pytorch_lightning import Trainer
 import os
+import argparse
 import json
-torch.backends.cudnn.enable =True
+
+torch.backends.cudnn.enable = True
 torch.backends.cudnn.benchmark = True
-os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
-torch.set_float32_matmul_precision('high')
+os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+torch.set_float32_matmul_precision("high")
 PATH = "./lightning_logs/version_1/checkpoints/epoch=29-step=330.ckpt"
+
 batch_size = 128
 epochs = 30
 dropout = 0.4
@@ -26,44 +28,53 @@ rnn_hidden = 768
 rnn_layer = 1
 class_num = 4
 lr = 0.001
+num = 0
 
-# todo：自定义数据集
+global token
+
+
+# todo: custom dataset
 class MydataSet(Dataset):
     def __init__(self, path, split):
-        self.dataset = load_dataset('csv', data_files=path, split=split)
+        self.dataset = load_dataset("csv", data_files=path, split=split)
+
     def __getitem__(self, item):
-        text = self.dataset[item]['new_message']
-        label = self.dataset[item]['label']
+        text = self.dataset[item]["new_message"]
+        label = self.dataset[item]["label"]
         return text, label
+
     def __len__(self):
         return len(self.dataset)
 
-# todo: 定义批处理函数
+
+# todo: define batch processing function
 def collate_fn(data):
     sents = [i[0] for i in data]
     labels = [i[1] for i in data]
-    # 分词并编码
+    # tokenize and encode
     data = token.batch_encode_plus(
-        batch_text_or_text_pairs=sents,  # 单个句子参与编码
-        truncation=True,  # 当句子长度大于max_length时,截断
-        padding='max_length',  # 一律补pad到max_length长度
+        batch_text_or_text_pairs=sents,  # single sentence encoding
+        truncation=True,  # truncate when sentence length exceeds max_length
+        padding="max_length",  # pad all to max_length
         max_length=200,
-        return_tensors='pt',  # 以pytorch的形式返回，可取值tf,pt,np,默认为返回list
+        return_tensors="pt",  # return in pytorch format, can be tf,pt,np, default returns list
         return_length=True,
     )
 
-    # input_ids:编码之后的数字
-    # attention_mask:是补零的位置是0,其他位置是1
-    input_ids = data['input_ids']  # input_ids 就是编码后的词
-    attention_mask = data['attention_mask']  # pad的位置是0,其他位置是1
-    token_type_ids = data['token_type_ids']  # (如果是一对句子)第一个句子和特殊符号的位置是0,第二个句子的位置是1
-    labels = torch.LongTensor(labels)  # 该批次的labels
+    # input_ids: encoded numbers
+    # attention_mask: padded positions are 0, other positions are 1
+    input_ids = data["input_ids"]  # input_ids are the encoded words
+    attention_mask = data["attention_mask"]  # pad positions are 0, other positions are 1
+    token_type_ids = data[
+        "token_type_ids"
+    ]  # (for sentence pairs) first sentence and special tokens are 0, second sentence is 1
+    labels = torch.LongTensor(labels)  # labels for this batch
 
     # print(data['length'], data['length'].max())
     return input_ids, attention_mask, token_type_ids, labels
 
 
-# todo: 定义模型，上游使用bert预训练，下游任务选择双向LSTM模型，最后加一个全连接层
+# todo: define model, upstream uses bert pretrained, downstream task uses bidirectional LSTM model, finally add a fully connected layer
 class BiLSTMClassifier(nn.Module):
     def __init__(self, drop, hidden_dim, output_dim):
         super(BiLSTMClassifier, self).__init__()
@@ -71,36 +82,46 @@ class BiLSTMClassifier(nn.Module):
         self.hidden_dim = hidden_dim
         self.output_dim = output_dim
 
-        # 加载bert中文模型,生成embedding层
-        self.embedding = BertModel.from_pretrained('bert-base-uncased')
-        # 去掉移至gpu
-        # 冻结上游模型参数(不进行预训练模型参数学习)
+        # load bert model, generate embedding layer
+        self.embedding = BertModel.from_pretrained("bert-base-uncased")
+        # remove move to gpu
+        # freeze upstream model parameters (do not learn pretrained model parameters)
         for param in self.embedding.parameters():
             param.requires_grad_(False)
-        # 生成下游RNN层以及全连接层
-        self.lstm = nn.LSTM(input_size=768, hidden_size=self.hidden_dim, num_layers=2, batch_first=True,
-                            bidirectional=True, dropout=self.drop)
+        # generate downstream RNN layer and fully connected layer
+        self.lstm = nn.LSTM(
+            input_size=768,
+            hidden_size=self.hidden_dim,
+            num_layers=2,
+            batch_first=True,
+            bidirectional=True,
+            dropout=self.drop,
+        )
         self.fc = nn.Linear(self.hidden_dim * 2, self.output_dim)
-        # 使用CrossEntropyLoss作为损失函数时，不需要激活。因为实际上CrossEntropyLoss将softmax-log-NLLLoss一并实现的。
+        # When using CrossEntropyLoss as loss function, no activation needed. Because CrossEntropyLoss actually implements softmax-log-NLLLoss together.
 
     def forward(self, input_ids, attention_mask, token_type_ids):
-        embedded = self.embedding(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
-        embedded = embedded.last_hidden_state  # 第0维才是我们需要的embedding,embedding.last_hidden_state = embedding[0]
+        embedded = self.embedding(
+            input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids
+        )
+        embedded = (
+            embedded.last_hidden_state
+        )  # dimension 0 is the embedding we need, embedding.last_hidden_state = embedding[0]
         out, (h_n, c_n) = self.lstm(embedded)
         output = torch.cat((h_n[-2, :, :], h_n[-1, :, :]), dim=1)
         output = self.fc(output)
         return output
 
-num = 0
-# todo: 定义pytorch lightning
+
+# todo: define pytorch lightning
 class BiLSTMLighting(pl.LightningModule):
 
     def __init__(self, drop, hidden_dim, output_dim):
         global num
         super(BiLSTMLighting, self).__init__()
-        self.model = BiLSTMClassifier(drop, hidden_dim, output_dim)  # 设置model
-        self.criterion = nn.CrossEntropyLoss()  # 设置损失函数
-        self.test_dataset = MydataSet(f'./data/archive/cstest/test{num}.csv', 'train')
+        self.model = BiLSTMClassifier(drop, hidden_dim, output_dim)  # setup model
+        self.criterion = nn.CrossEntropyLoss()  # setup loss function
+        self.test_dataset = MydataSet(f"./data/archive/cstest/test{num}.csv", "train")
 
     def configure_optimizers(self):
         optimizer = optim.AdamW(self.parameters(), lr=lr)
@@ -110,24 +131,29 @@ class BiLSTMLighting(pl.LightningModule):
         return self.model(input_ids, attention_mask, token_type_ids)
 
     def train_dataloader(self):
-        train_loader = DataLoader(dataset=self.train_dataset, batch_size=batch_size, collate_fn=collate_fn,
-                                  shuffle=True)
+        train_loader = DataLoader(
+            dataset=self.train_dataset, batch_size=batch_size, collate_fn=collate_fn, shuffle=True
+        )
         return train_loader
 
     def training_step(self, batch, batch_idx):
         input_ids, attention_mask, token_type_ids, labels = batch  # x, y = batch
         y = one_hot(labels, num_classes=4)
-        # 将one_hot_labels类型转换成float
+        # convert one_hot_labels type to float
         y = y.to(dtype=torch.float)
         # forward pass
         y_hat = self.model(input_ids, attention_mask, token_type_ids)
-        y_hat = y_hat.squeeze()  # 将[128, 1, 3]挤压为[128,3]
+        y_hat = y_hat.squeeze()  # squeeze [128, 1, 3] to [128,3]
         loss = self.criterion(y_hat, y)  # criterion(input, target)
-        self.log('train_loss', loss, prog_bar=True, logger=True, on_step=True, on_epoch=True)  # 将loss输出在控制台
-        return loss  # 必须把log返回回去才有用
+        self.log(
+            "train_loss", loss, prog_bar=True, logger=True, on_step=True, on_epoch=True
+        )  # output loss to console
+        return loss  # must return log for it to be useful
 
     def val_dataloader(self):
-        val_loader = DataLoader(dataset=self.val_dataset, batch_size=batch_size, collate_fn=collate_fn, shuffle=False)
+        val_loader = DataLoader(
+            dataset=self.val_dataset, batch_size=batch_size, collate_fn=collate_fn, shuffle=False
+        )
         return val_loader
 
     def validation_step(self, batch, batch_idx):
@@ -138,16 +164,18 @@ class BiLSTMLighting(pl.LightningModule):
         y_hat = self.model(input_ids, attention_mask, token_type_ids)
         y_hat = y_hat.squeeze()
         loss = self.criterion(y_hat, y)
-        self.log('val_loss', loss, prog_bar=False, logger=True, on_step=True, on_epoch=True)
+        self.log("val_loss", loss, prog_bar=False, logger=True, on_step=True, on_epoch=True)
         return loss
 
     def test_dataloader(self):
-        test_loader = DataLoader(dataset=self.test_dataset, batch_size=batch_size, collate_fn=collate_fn, shuffle=False)
+        test_loader = DataLoader(
+            dataset=self.test_dataset, batch_size=batch_size, collate_fn=collate_fn, shuffle=False
+        )
         return test_loader
 
     def test_step(self, batch, batch_idx):
         input_ids, attention_mask, token_type_ids, labels = batch
-        target = labels  # 用于待会儿计算acc和f1-score
+        target = labels  # used for calculating acc and f1-score later
         y = one_hot(target, num_classes=4)
         y = y.to(dtype=torch.float)
         # forward pass
@@ -155,25 +183,36 @@ class BiLSTMLighting(pl.LightningModule):
         y_hat = y_hat.squeeze()
         pred = torch.argmax(y_hat, dim=1)
         print(pred)
-        with open('preds_csharp.json', 'a') as f:
-            json.dump(pred.cpu().numpy().tolist(), f)  # 先将tensor转为numpy，再转为list保存
+        with open("preds_csharp.json", "a") as f:
+            json.dump(
+                pred.cpu().numpy().tolist(), f
+            )  # first convert tensor to numpy, then to list for saving
 
         acc = (pred == target).float().mean()
 
         loss = self.criterion(y_hat, y)
-        self.log('loss', loss)
-        # task: Literal["binary", "multiclass", "multilabel"],对应[二分类，多分类，多标签]
-        #  average=None分别输出各个类别, 不加默认算平均
+        self.log("loss", loss)
+        # task: Literal["binary", "multiclass", "multilabel"], corresponding to [binary classification, multiclass, multilabel]
+        #  average=None outputs each class separately, without it defaults to average
 
 
 def test():
-    # 加载之前训练好的最优模型参数
-    model = BiLSTMLighting.load_from_checkpoint(checkpoint_path=PATH,
-                                                drop=dropout, hidden_dim=rnn_hidden, output_dim=class_num)
+    # load previously trained optimal model parameters
+    model = BiLSTMLighting.load_from_checkpoint(
+        checkpoint_path=PATH, drop=dropout, hidden_dim=rnn_hidden, output_dim=class_num
+    )
     trainer = Trainer(fast_dev_run=False)
     result = trainer.test(model)
     print(result)
-token = BertTokenizer.from_pretrained('bert-base-uncased')
-for i in range(5):
-    test()
-    num += 1
+
+
+def main():
+    global token
+    token = BertTokenizer.from_pretrained("bert-base-uncased")
+    for i in range(5):
+        test()
+        num += 1
+
+
+if __name__ == "__main__":
+    main()
--- Models & Metrics/GPT_without_example.py
+++ Models & Metrics/GPT_without_example.py
@@ -8,41 +8,48 @@ import traceback
 from openai import OpenAI
 import time
 
-lan = 'py.jsonl'
-output_filename = 'pygptnoexample.jsonl'
+lan = "py.jsonl"
+output_filename = "pygptnoexample.jsonl"
 key_list = []
+
 
 def is_camel_case(s):
     return s != s.lower() and s != s.upper() and "_" not in s
 
+
 def to_Underline(x):
     """转空格命名"""
-    return re.sub('(?<=[a-z])[A-Z]|(?<!^)[A-Z](?=[a-z])', ' \g<0>', x).lower()
+    return re.sub("(?<=[a-z])[A-Z]|(?<!^)[A-Z](?=[a-z])", " \g<0>", x).lower()
+
 
 def remove_between_identifiers(text, identifier_start, identifier_end):
     # 定义正则表达式模式
-    pattern = f'(?<={identifier_start}).*?(?={identifier_end})'
+    pattern = f"(?<={identifier_start}).*?(?={identifier_end})"
 
     # 使用re.sub方法替换匹配到的部分为空字符串
-    result = re.sub(pattern, '', text)
-    if identifier_start == 'mmm a':
-        result = result.replace('mmm a<nl>', '')
-    if identifier_start == 'ppp b':
-        result = result.replace('ppp b<nl>', '')
-        result = result.replace('<nl>', '\n')
-    result = result.replace(' . ', '.')
-    result = result.replace('  ', '.')
-    result = result.replace(' = ', '=')
-    result = result.replace(' ; ', ';')
-    result = result.replace(' (', '(')
-    result = result.replace(') ', ')')
+    result = re.sub(pattern, "", text)
+    if identifier_start == "mmm a":
+        result = result.replace("mmm a<nl>", "")
+    if identifier_start == "ppp b":
+        result = result.replace("ppp b<nl>", "")
+        result = result.replace("<nl>", "\n")
+    result = result.replace(" . ", ".")
+    result = result.replace("  ", ".")
+    result = result.replace(" = ", "=")
+    result = result.replace(" ; ", ";")
+    result = result.replace(" (", "(")
+    result = result.replace(") ", ")")
     return result
+
+
 def get_tokens(text):
     tokens = nltk.word_tokenize(text)
     if len(tokens) > 1024:
-        return ' '.join(tokens[:1024])
+        return " ".join(tokens[:1024])
     else:
-        return ' '.join(tokens)
+        return " ".join(tokens)
+
+
 def process_diff(diff):
     wordsGPT = diff.split()
     msgGPT_list = []
@@ -54,18 +61,25 @@ def process_diff(diff):
                 msgGPT_list.append(wordGPT)
         else:
             msgGPT_list.append(wordGPT)
-    diff = ' '.join(msgGPT_list)
+    diff = " ".join(msgGPT_list)
 
-    result = remove_between_identifiers(diff, 'mmm a', '<nl>')
-    diff = remove_between_identifiers(result, 'ppp b', '<nl>')
+    result = remove_between_identifiers(diff, "mmm a", "<nl>")
+    diff = remove_between_identifiers(result, "ppp b", "<nl>")
 
     return get_tokens(diff)
 
 
 # 打开JSONL文件并读取数据
-with open(lan, 'r',encoding='utf8') as f:
+with open(lan, "r", encoding="utf8") as f:
     json_data = f.readlines()
-data = {"diff_id": 0, "msg": f"0", "msgGPT": f"0", "METEOR Score": f"0", "BLEU Score": f"0", "ROUGE-L Score": f"0"}
+data = {
+    "diff_id": 0,
+    "msg": f"0",
+    "msgGPT": f"0",
+    "METEOR Score": f"0",
+    "BLEU Score": f"0",
+    "ROUGE-L Score": f"0",
+}
 
 # 遍历 JSON 数据，提取并存储 diff 和 msg
 num = 0
@@ -85,11 +99,11 @@ for item in json_data:
         data = json.loads(item)
 
         # 提取 diff 和 msg
-        diff_id = data['diff_id']
-        diff = data['diff']
-        result = remove_between_identifiers(diff, 'mmm a', '<nl>')
-        diff = get_tokens(remove_between_identifiers(result, 'ppp b', '<nl>'))
-        msg = data['msg']
+        diff_id = data["diff_id"]
+        diff = data["diff"]
+        result = remove_between_identifiers(diff, "mmm a", "<nl>")
+        diff = get_tokens(remove_between_identifiers(result, "ppp b", "<nl>"))
+        msg = data["msg"]
         words = msg.split()
         msg_list = []
         for word in words:
@@ -100,7 +114,7 @@ for item in json_data:
                     msg_list.append(word)
             else:
                 msg_list.append(word)
-        msg = ' '.join(msg_list)
+        msg = " ".join(msg_list)
         # Example usage:
         if num < 4:
             num += 1
@@ -111,14 +125,16 @@ for item in json_data:
                 model="gpt-3.5-turbo",
                 messages=[
                     {"role": "system", "content": "You are a programmer who makes the above code changes."},
-                    {"role": "user",
-                     "content": f'''{diff}\n Please write a commit message that contains only one simple sentence for the above code change.\n'''},
+                    {
+                        "role": "user",
+                        "content": f"""{diff}\n Please write a commit message that contains only one simple sentence for the above code change.\n""",
+                    },
                     # {"role": "user", "content": "Use END_OF_CASE to finish your answer.\n"},
                 ],
                 max_tokens=50,
                 temperature=0.8,
                 n=30,
-                top_p=0.95
+                top_p=0.95,
             )
             num_answers = 30
             msgGPTs = []
@@ -134,7 +150,7 @@ for item in json_data:
                             msgGPT_list.append(wordGPT)
                     else:
                         msgGPT_list.append(wordGPT)
-                msgGPT = ' '.join(msgGPT_list)
+                msgGPT = " ".join(msgGPT_list)
                 msgGPTs.append(msgGPT)
             print(msgGPTs)
 
@@ -142,16 +158,16 @@ for item in json_data:
             data = {"diff_id": diff_id, "msg": f"{msg}"}
             for i in range(30):
                 data[f"msgGPT{i}"] = f"{msgGPTs[i]}"
-            with open(output_filename, 'a') as f:
+            with open(output_filename, "a") as f:
                 json.dump(data, f)
-                f.write('\n')
+                f.write("\n")
             break
         except:
             traceback.print_exc()
             data = {"diff_id": diff_id, "msg": f"0", "msgGPT": f"0"}
-            with open(output_filename, 'a') as f:
+            with open(output_filename, "a") as f:
                 json.dump(data, f)
-                f.write('\n')
+                f.write("\n")
             time.sleep(1)
             attempts += 1
             if attempts == 5:
